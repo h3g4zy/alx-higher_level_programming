4537b3c402e5ecc88451562bc75bb4beecd22b09
h3g4zy <h3g4zy@gmail.com>
2023-11-01 02:31:10 +0200

    19963

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char_code in range(ord('a'), ord('z') + 1):
-    print(chr(char_code), end= '')
+alphabet = "".join([chr(ord('a') + i) for i in range(26)])
+print(f"{alphabet}", end='')
